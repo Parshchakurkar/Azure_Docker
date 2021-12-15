6b2a4c40474fdf6c73367e7b4e9660bc24b66297
parsh <parshchakurkar@gmail.com>
2021-12-15 08:03:09 -0700

    main.tf updated

--- main.tf
+++ main.tf
@@ -26,10 +26,31 @@ resource "azurerm_kubernetes_cluster" "dockercluster" {
   resource_group_name = azurerm_resource_group.dockerresource.name
   dns_prefix          = "cluster1"
 
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data= file(var.ssh_public_key)
+    }
+  }
+
   default_node_pool {
     name       = "default"
     node_count = 3
     vm_size    = "Standard_D2_v2"
   }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = "kubenet"
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
 }
